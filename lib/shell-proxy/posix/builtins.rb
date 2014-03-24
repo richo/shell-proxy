abd646cbd17ffac00fb1d40fd6e26adcdc2cc742
Richo Healey <richo@psych0tik.net>
2014-03-24 11:57:09 +1100

    Implement getopt arg validation

--- lib/shell-proxy/posix/builtins.rb
+++ lib/shell-proxy/posix/builtins.rb
@@ -1,6 +1,20 @@
 module PosixProxy
+  class InvalidOption < Exception
+  end
+
   module Builtins
-    def builtin(name)
+    VALIDATE_ARGS = lambda { |opts, allowed_opts|
+      opts.each do |k, v|
+        unless allowed_opts.keys.include? k
+          raise InvalidOption "No such option #{k}"
+        end
+
+        if v && !["?", 1].include?(allowed_opts[k])
+          raise InvalidOption "#{k} does not accept an argument"
+        end
+      end
+    }
+    def builtin(name, allowed_opts=nil)
       self.send(:define_method, name) do |*args|
         opts = case args[-1]
                when Hash
@@ -8,6 +22,8 @@ module PosixProxy
                else
                  {}
                end
+
+        VALIDATE_ARGS.call(opts, allowed_opts) if allowed_opts
 
         stub = CmdStub.new(@cmd_buffer)
 
